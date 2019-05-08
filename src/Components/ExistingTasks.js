6709a639c5db0395a1cc597b8bbaa347f69fc0e9
Portia Patterson <portialorrainepatterson@gmail.com>
2019-05-08 17:33:03 +0100

    Got errors...

--- src/Components/ExistingTasks.js
+++ src/Components/ExistingTasks.js
@@ -10,13 +10,13 @@ class ExistingTasks extends Component {
                 <div className="col-sm-12 col-md-6">
                     {/* this displays any task details given */}
                     {this.props.task.currentTaskDescr}
-                    {/* -{this.props.task.completed ? "Yes":"No"} */}
+                    {/* -{this.props.task.completed ? "Yes":"No"} */} 
                 </div>
                 <div className="col-sm-6 col-md-3">
                 {/* this passes the id of the record to be completed to the completeTask function 
                 on the App.js.NOTE THE onClick is done in 1 go instead of going to another function to send 
                 the details to the App.js file as is done on the NewTask component. This way is neater */}
-                    <button type="button" //onClick={() => this.props.completeTask(this.props.task.id)}
+                    <button type="button" onClick={() => this.props.completeTask(this.props.task.id)}
                     >DONE</button>
                 </div>
                 <div className="col-sm-6 col-md-3" >
